56efccd80ef2a5d58afd6c1bbea86ef83b0188d6
reyart <ra.llenes@gmail.com>
2016-03-28 23:25:02 -0400

    Completed basic UI required for prototype

--- ZpOptimizer/ZpOptimizerUI/ZpOptimizerUI.Designer.cs
+++ ZpOptimizer/ZpOptimizerUI/ZpOptimizerUI.Designer.cs
@@ -37,16 +37,38 @@
             this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.splitContainerMain = new System.Windows.Forms.SplitContainer();
             this.splitContainerTop = new System.Windows.Forms.SplitContainer();
-            this.folderListBox = new System.Windows.Forms.ListBox();
-            this.browseButton = new System.Windows.Forms.Button();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.listBoxFolders = new System.Windows.Forms.ListBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.buttonApply = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.radioButtonOptimized = new System.Windows.Forms.RadioButton();
+            this.radioButtonMaxComp = new System.Windows.Forms.RadioButton();
+            this.radioButtonUncompressed = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bottomTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.progressBarGame = new System.Windows.Forms.ProgressBar();
+            this.progressBarFile = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
             this.menuStrip.SuspendLayout();
             this.statusStrip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainerMain)).BeginInit();
             this.splitContainerMain.Panel1.SuspendLayout();
+            this.splitContainerMain.Panel2.SuspendLayout();
             this.splitContainerMain.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainerTop)).BeginInit();
             this.splitContainerTop.Panel1.SuspendLayout();
+            this.splitContainerTop.Panel2.SuspendLayout();
             this.splitContainerTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.bottomTableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip
@@ -71,7 +93,7 @@
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             // 
             // helpToolStripMenuItem
@@ -95,6 +117,7 @@
             this.statusStrip.Location = new System.Drawing.Point(0, 419);
             this.statusStrip.Name = "statusStrip";
             this.statusStrip.Size = new System.Drawing.Size(624, 22);
+            this.statusStrip.SizingGrip = false;
             this.statusStrip.TabIndex = 1;
             this.statusStrip.Text = "statusStrip";
             // 
@@ -116,6 +139,12 @@
             // splitContainerMain.Panel1
             // 
             this.splitContainerMain.Panel1.Controls.Add(this.splitContainerTop);
+            // 
+            // splitContainerMain.Panel2
+            // 
+            this.splitContainerMain.Panel2.Controls.Add(this.label2);
+            this.splitContainerMain.Panel2.Controls.Add(this.label1);
+            this.splitContainerMain.Panel2.Controls.Add(this.bottomTableLayoutPanel);
             this.splitContainerMain.Size = new System.Drawing.Size(624, 395);
             this.splitContainerMain.SplitterDistance = 250;
             this.splitContainerMain.SplitterWidth = 1;
@@ -131,33 +160,172 @@
             // 
             // splitContainerTop.Panel1
             // 
-            this.splitContainerTop.Panel1.Controls.Add(this.browseButton);
-            this.splitContainerTop.Panel1.Controls.Add(this.folderListBox);
+            this.splitContainerTop.Panel1.Controls.Add(this.buttonBrowse);
+            this.splitContainerTop.Panel1.Controls.Add(this.listBoxFolders);
+            // 
+            // splitContainerTop.Panel2
+            // 
+            this.splitContainerTop.Panel2.Controls.Add(this.splitContainer1);
             this.splitContainerTop.Size = new System.Drawing.Size(624, 250);
             this.splitContainerTop.SplitterDistance = 312;
             this.splitContainerTop.SplitterWidth = 1;
             this.splitContainerTop.TabIndex = 0;
             // 
-            // folderListBox
+            // buttonBrowse
             // 
-            this.folderListBox.FormattingEnabled = true;
-            this.folderListBox.Items.AddRange(new object[] {
+            this.buttonBrowse.Location = new System.Drawing.Point(4, 223);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowse.TabIndex = 1;
+            this.buttonBrowse.Text = "Browse...";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            // 
+            // listBoxFolders
+            // 
+            this.listBoxFolders.FormattingEnabled = true;
+            this.listBoxFolders.Items.AddRange(new object[] {
             "C:\\Program Files (x86)\\TRICENTIS",
             "C:\\Program Files\\TortoiseSVN",
             "C:\\Program Files\\WinRAR"});
-            this.folderListBox.Location = new System.Drawing.Point(4, 4);
-            this.folderListBox.Name = "folderListBox";
-            this.folderListBox.Size = new System.Drawing.Size(305, 212);
-            this.folderListBox.TabIndex = 0;
+            this.listBoxFolders.Location = new System.Drawing.Point(4, 4);
+            this.listBoxFolders.Name = "listBoxFolders";
+            this.listBoxFolders.Size = new System.Drawing.Size(305, 212);
+            this.listBoxFolders.TabIndex = 0;
             // 
-            // browseButton
+            // splitContainer1
             // 
-            this.browseButton.Location = new System.Drawing.Point(4, 223);
-            this.browseButton.Name = "browseButton";
-            this.browseButton.Size = new System.Drawing.Size(75, 23);
-            this.browseButton.TabIndex = 1;
-            this.browseButton.Text = "Browse...";
-            this.browseButton.UseVisualStyleBackColor = true;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(311, 250);
+            this.splitContainer1.SplitterDistance = 168;
+            this.splitContainer1.SplitterWidth = 1;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.buttonApply);
+            this.splitContainer2.Panel2.Controls.Add(this.flowLayoutPanel1);
+            this.splitContainer2.Size = new System.Drawing.Size(311, 168);
+            this.splitContainer2.SplitterDistance = 182;
+            this.splitContainer2.SplitterWidth = 1;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // buttonApply
+            // 
+            this.buttonApply.Location = new System.Drawing.Point(6, 111);
+            this.buttonApply.Name = "buttonApply";
+            this.buttonApply.Size = new System.Drawing.Size(108, 34);
+            this.buttonApply.TabIndex = 1;
+            this.buttonApply.Text = "Apply";
+            this.buttonApply.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.radioButtonOptimized);
+            this.flowLayoutPanel1.Controls.Add(this.radioButtonMaxComp);
+            this.flowLayoutPanel1.Controls.Add(this.radioButtonUncompressed);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 4);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(119, 86);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // radioButtonOptimized
+            // 
+            this.radioButtonOptimized.AutoSize = true;
+            this.radioButtonOptimized.Location = new System.Drawing.Point(3, 3);
+            this.radioButtonOptimized.Name = "radioButtonOptimized";
+            this.radioButtonOptimized.Size = new System.Drawing.Size(71, 17);
+            this.radioButtonOptimized.TabIndex = 0;
+            this.radioButtonOptimized.TabStop = true;
+            this.radioButtonOptimized.Text = "Optimized";
+            this.radioButtonOptimized.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonMaxComp
+            // 
+            this.radioButtonMaxComp.AutoSize = true;
+            this.radioButtonMaxComp.Location = new System.Drawing.Point(3, 26);
+            this.radioButtonMaxComp.Name = "radioButtonMaxComp";
+            this.radioButtonMaxComp.Size = new System.Drawing.Size(108, 17);
+            this.radioButtonMaxComp.TabIndex = 1;
+            this.radioButtonMaxComp.TabStop = true;
+            this.radioButtonMaxComp.Text = "Max Compression";
+            this.radioButtonMaxComp.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonUncompressed
+            // 
+            this.radioButtonUncompressed.AutoSize = true;
+            this.radioButtonUncompressed.Location = new System.Drawing.Point(3, 49);
+            this.radioButtonUncompressed.Name = "radioButtonUncompressed";
+            this.radioButtonUncompressed.Size = new System.Drawing.Size(96, 17);
+            this.radioButtonUncompressed.TabIndex = 2;
+            this.radioButtonUncompressed.TabStop = true;
+            this.radioButtonUncompressed.Text = "Uncompressed";
+            this.radioButtonUncompressed.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(277, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FileName.vpk";
+            // 
+            // bottomTableLayoutPanel
+            // 
+            this.bottomTableLayoutPanel.ColumnCount = 1;
+            this.bottomTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.bottomTableLayoutPanel.Controls.Add(this.progressBarGame, 0, 1);
+            this.bottomTableLayoutPanel.Controls.Add(this.progressBarFile, 0, 0);
+            this.bottomTableLayoutPanel.Location = new System.Drawing.Point(4, 5);
+            this.bottomTableLayoutPanel.Name = "bottomTableLayoutPanel";
+            this.bottomTableLayoutPanel.RowCount = 3;
+            this.bottomTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.bottomTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.bottomTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.bottomTableLayoutPanel.Size = new System.Drawing.Size(617, 136);
+            this.bottomTableLayoutPanel.TabIndex = 0;
+            // 
+            // progressBarGame
+            // 
+            this.progressBarGame.Location = new System.Drawing.Point(3, 49);
+            this.progressBarGame.Name = "progressBarGame";
+            this.progressBarGame.Size = new System.Drawing.Size(611, 40);
+            this.progressBarGame.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBarGame.TabIndex = 1;
+            this.progressBarGame.Value = 78;
+            // 
+            // progressBarFile
+            // 
+            this.progressBarFile.Location = new System.Drawing.Point(3, 3);
+            this.progressBarFile.Name = "progressBarFile";
+            this.progressBarFile.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.progressBarFile.Size = new System.Drawing.Size(611, 40);
+            this.progressBarFile.TabIndex = 0;
+            this.progressBarFile.Value = 43;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(276, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "GameName";
             // 
             // ZpOptimizerUI
             // 
@@ -167,6 +335,7 @@
             this.Controls.Add(this.splitContainerMain);
             this.Controls.Add(this.statusStrip);
             this.Controls.Add(this.menuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MainMenuStrip = this.menuStrip;
             this.MaximizeBox = false;
             this.Name = "ZpOptimizerUI";
@@ -176,11 +345,23 @@
             this.statusStrip.ResumeLayout(false);
             this.statusStrip.PerformLayout();
             this.splitContainerMain.Panel1.ResumeLayout(false);
+            this.splitContainerMain.Panel2.ResumeLayout(false);
+            this.splitContainerMain.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainerMain)).EndInit();
             this.splitContainerMain.ResumeLayout(false);
             this.splitContainerTop.Panel1.ResumeLayout(false);
+            this.splitContainerTop.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainerTop)).EndInit();
             this.splitContainerTop.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.bottomTableLayoutPanel.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -197,8 +378,20 @@
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
         private System.Windows.Forms.SplitContainer splitContainerMain;
         private System.Windows.Forms.SplitContainer splitContainerTop;
-        private System.Windows.Forms.Button browseButton;
-        private System.Windows.Forms.ListBox folderListBox;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.ListBox listBoxFolders;
+        private System.Windows.Forms.TableLayoutPanel bottomTableLayoutPanel;
+        private System.Windows.Forms.ProgressBar progressBarGame;
+        private System.Windows.Forms.ProgressBar progressBarFile;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Button buttonApply;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.RadioButton radioButtonOptimized;
+        private System.Windows.Forms.RadioButton radioButtonMaxComp;
+        private System.Windows.Forms.RadioButton radioButtonUncompressed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
